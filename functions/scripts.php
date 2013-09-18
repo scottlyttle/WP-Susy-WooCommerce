cb8b496cbf29917802125d7f9308275dbfe08614
Scott Lyttle <scott.lyttle@gmail.com>
2013-09-18 14:56:13 +1000

    Added Gravity Forms customiser

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,8 +1,13 @@
 <?php
 function yabtfw_scripts() {
+	// Styles
+	wp_enqueue_style( 'gravityforms', get_stylesheet_uri() . '/css/gravityforms.css', array(), '1.0');
+
+	// Scripts
 	wp_enqueue_script('jquery');
-	wp_enqueue_script('plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'));
-	wp_enqueue_script('functions', get_template_directory_uri() . '/js/functions.js', array('jquery'));
+	wp_enqueue_script('plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), true);
+	wp_enqueue_script('functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), true);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
