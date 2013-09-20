70a98bde1a42639318efdb9d7e351667b41db00e
Scott Lyttle <scott.lyttle@gmail.com>
2013-09-20 17:37:30 +1000

    Updated Gravity Forms SCSS

--- functions/misc.php
+++ functions/misc.php
@@ -4,3 +4,23 @@ function register_editor_stylesheet() {
     add_editor_style( 'css/editor-style.css' );
 }
 add_action( 'init', 'register_editor_stylesheet' );
+
+// Custom login page logo
+function theme_login_logo() {
+    echo 
+    '<style type="text/css">
+        .login h1 a { 
+			background-image:url('.get_bloginfo('template_directory').'/img/login-logo.png) !important; 
+			background-size: 274px auto !important;
+			height: 125px !important;
+			width: 320px !important;
+		}
+    </style>';
+}
+add_action('login_head', 'theme_login_logo');
+
+// WordPress Change Login page logo link
+function change_login_page_url( $url ) {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'change_login_page_url' );
